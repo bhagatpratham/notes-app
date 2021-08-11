bc1c6d454d3bc259bdb6918363a8f81a397d5da5
Pratham <bhagatpratham101@gmail.com>
2021-08-11 16:40:40 +0530

    Delete playground directory

--- playground/1-json.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-// const books = {
-//   title: "The 5am Club",
-//   author: "Robin Sharma",
-// };
-
-// const stringJSON = JSON.stringify(books);
-// fs.writeFileSync("1-json.json", stringJSON);
